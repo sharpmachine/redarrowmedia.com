89d33ad1be6286ef6921c1706132fa4197a00193
Jesse Kade <jessekade@gmail.com>
2012-04-24 11:08:53 -0700

    added 503

---
+++ wp-content/themes/greyzed/503.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Down for Maintenance</title>
+
+<style type="text/css">
+	body
+	{
+		background-color: #fff;
+		color: #000;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+	
+	#logo
+	{
+		width: 300px;
+		margin: 50px auto;
+	}
+	
+	p
+	{
+		text-align: center;
+	}
+	
+</style>
+
+</head>
+<body>
+		
+	<div id="logo">
+		<img src="http://www.redarrowmedia.com/wp-content/themes/greyzed/images/logo.png" width="300" height="200" alt="Logo">
+		<p>We are currently undergoing routine maintenance.  Please check back soon.</p>
+	</div>
+		
+</body>
+</html>
+
